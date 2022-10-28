c34629b65c4773dd4dde57c2ae8404e38381ef14
JohnGrubba <nicjontrickshots@gmail.com>
2022-10-28 12:29:21 +0200

    Song Searcher

---
+++ docs/script.js
@@ -0,0 +1,30 @@
+fetch("https://raw.githubusercontent.com/JohnGrubba/TromboneChampAutomod/main/songs.json")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        out = "";
+        id = 0;
+        data.forEach(element => {
+            out += "<li><a class='id'>" + id + "</a><a class='name'>" + element.song_name + "</a></li>"
+            id++;
+        });
+        document.getElementById("songs").innerHTML = out;
+    });
+
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("songs");
+    li = ul.getElementsByTagName('li');
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[1];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+    console.log("Filtered" + input.value.toUpperCase())
+}
